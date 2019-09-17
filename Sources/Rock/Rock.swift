8c4a80f4af8b32f527f987526ef4038e549da0b2
Dohyeon Kim <dohyeon.kim@life.church>
2019-09-17 14:50:48 -0500

    Make Rock class public

--- Sources/Rock/Rock.swift
+++ Sources/Rock/Rock.swift
@@ -1,13 +1,13 @@
 import Foundation
 
 public class Rock {
-    static let API = RockAPIClient()
+    public static let API = RockAPIClient()
 
-    static func configure(baseURL: URL?) {
+    public static func configure(baseURL: URL?) {
         API.baseURL = baseURL
     }
     
-    static func assign(token: String?) {
+    public static func assign(token: String?) {
         API.token = token
     }
 }
