0b2a2773deeea4f1c55458aa0624d0ed562a9c32
Dohyeon Kim <dohyeon.kim@life.church>
2019-09-17 14:52:47 -0500

    Make APIClient public

--- Sources/Rock/API/APIClient.swift
+++ Sources/Rock/API/APIClient.swift
@@ -3,13 +3,13 @@ import Foundation
 import Combine
 #endif
 
-protocol APIClient {
+public protocol APIClient {
     var baseURL: URL? { get}
     
-    func send<T: APIRequest>(_ request: T, completion: @escaping ResultCallback<T.Response>)
+    public func send<T: APIRequest>(_ request: T, completion: @escaping ResultCallback<T.Response>)
     
     #if canImport(Combine)
     @available(OSX 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
-    func publisher<T: APIRequest>(_ request: T) -> AnyPublisher<T.Response, Error>?
+    public func publisher<T: APIRequest>(_ request: T) -> AnyPublisher<T.Response, Error>?
     #endif
 }
