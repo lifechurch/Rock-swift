cb43d4caded4baf7cda15ad1a562d71024687e7e
Dohyeon Kim <dohyeon.kim@life.church>
2019-09-23 20:29:36 -0500

    Make StreakType hashable

--- Sources/Rock/Responses/StreakType.swift
+++ Sources/Rock/Responses/StreakType.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public struct StreakType: Codable {
+public struct StreakType: Codable, Hashable {
     public let id: Int
     public let name: String
     public let isActive: Bool
