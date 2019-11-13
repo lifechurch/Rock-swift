2da5aa54d6e461417b690f91b006c86e252808a0
Dohyeon Kim <dohyeon.kim@life.church>
2019-11-13 12:52:02 -0600

    Make member variables public

--- Sources/Rock/Responses/InteractionStatistics.swift
+++ Sources/Rock/Responses/InteractionStatistics.swift
@@ -1,10 +1,10 @@
 import Foundation
 
 public struct InteractionStatistics: Codable {
-    private(set) var interactionsAllTime: Int
-    private(set) var interactionsThatDay: Int
-    private(set) var interactionsThatMonth: Int
-    private(set) var interactionsThatYear: Int
+    public var interactionsAllTime: Int
+    public var interactionsThatDay: Int
+    public var interactionsThatMonth: Int
+    public var interactionsThatYear: Int
     
     public mutating func increment() {
         interactionsAllTime += 1
